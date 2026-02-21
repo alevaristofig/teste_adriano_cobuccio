e2299d6a3fc6a1da8d9137f3f04a046423457306
Alexandre <alevaristofig@gmail.com>
2026-02-21 19:20:28 -0300

    Refatoração no repositorio carteira, mudança dos metodos, acrescentou os medotos de crud

--- app/Repository/CarteiraRepository.php
+++ app/Repository/CarteiraRepository.php
@@ -6,7 +6,13 @@ use App\Http\Requests\DepositoRequest;
 use App\Models\Carteira;
 
 interface CarteiraRepository {
-    public function depositar(CarteiraRequest $request): Carteira;
+    /*public function depositar(CarteiraRequest $request): Carteira;
     public function transferir(CarteiraRequest $request): Carteira;
-    public function constestar(CarteiraRequest $request): String;
+    public function constestar(CarteiraRequest $request): String;*/
+
+    public function salvar(CarteiraRequest $request);
+    public function listar(): Collection;
+    public function buscar(int $id): Carteira | null;
+    public function atualizar(int $id, CarteiraRequest $request): Carteira | null;
+    public function deletar(int $id): void;
 }
