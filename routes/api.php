54e9c7b34c2d28dfcca98693e59f95ff5ba9e441
Alexandre <alevaristofig@gmail.com>
2026-02-22 13:08:44 -0300

    Implementação da rota usuario

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,21 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\UserController;
+
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::prefix('usuario')->group(function() {
+    Route::group([
+        'as' => 'usuario',
+        //'middleware'=> \Tymon\JWTAuth\Http\Middleware\Authenticate::class
+    ], function() {      
+       // Route::get('/produto',[ProdutoController::class,'listar']);
+        Route::post('/usuario',[UserController::class,'salvar']);
+       // Route::put('/produto/{id}',[ProdutoController::class,'atualizar']);
+       // Route::get('/produto/{id}',[ProdutoController::class,'buscar']);
+       // Route::delete('/produto/{id}',[ProdutoController::class,'deletar']);
+    });
+});
